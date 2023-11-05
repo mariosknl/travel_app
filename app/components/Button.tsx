06c746f41ab4ddde8e51fffa5371567f08123fb2
Marios Knl <mariosknl@outlook.com>
2023-11-05 17:25:44 +0200

    hero section

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 	type: "button" | "submit";
 	title: string;
 	icon?: string;
-	variant?: "btn_dark_green" | "btn_white" | "btn_green";
+	variant?: "btn_dark_green" | "btn_white_text" | "btn_green";
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
